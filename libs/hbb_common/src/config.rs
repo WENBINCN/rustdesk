c9442f3c8beb28ca83f507e33a21be7981003e20
Eason <115969519+WENBINCN@users.noreply.github.com>
2024-11-12 17:39:59 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["zz.xinbin.fun"];
-pub const PUBLIC_RS_PUB_KEY: &str = "123123123123123123123=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["zhyc.xinbin.fun"];
+pub const PUBLIC_RS_PUB_KEY: &str = "Qjbwkur51aj95nKHiueoxbFXkVxADHhpAEDHwvVKCdY=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
